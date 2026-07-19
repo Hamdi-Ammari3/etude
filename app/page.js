c066009c3650be56c66ecf69781468df43fa3539
Hamdi Ammari <elammari2022@gmail.com>
2026-07-19 14:09:20 +0100

    new update version 3

--- app/page.js
+++ app/page.js
@@ -44,6 +44,7 @@ export default function HomePage() {
     return null;
   }
 
+  //Grade progress
   function getGradeProgress(gradeId) {
     const subjects = subjectsByGrade.get(gradeId);
     if (!subjects || subjects.length === 0) return 0;
